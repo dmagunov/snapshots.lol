b713fbc2819732ad171c793be34faf9885ea7ee9
Dmitry Magunov <dmagunov@gmail.com>
2023-04-28 23:54:10 +0200

    fix: remove unused plugin from next config

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -10,27 +10,25 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withTM(
-  withPWA({
-    compiler: {
-      styledComponents: true,
-    },
-    transpilePackages: ["@thenftsnapshot/hardhat", "@thenftsnapshot/themes"],
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      });
+module.exports = withPWA({
+  compiler: {
+    styledComponents: true,
+  },
+  transpilePackages: ["@thenftsnapshot/hardhat", "@thenftsnapshot/themes"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
 
-      return config;
-    },
-    pageExtensions: ["page.tsx", "page.ts"],
-  })
-);
+    return config;
+  },
+  pageExtensions: ["page.tsx", "page.ts"],
+});
